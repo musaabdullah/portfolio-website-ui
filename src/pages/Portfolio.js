ee45cafecf4baaca89679ed0a4bab011789403f4
musaabdullah <musaabdalwahed@gmail.com>
2021-03-14 11:50:28 +0200

    fire work

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,10 +2,23 @@ import React from 'react'
 import './portfolio.css';
 import * as FIcon from "react-icons/fa";
 import IMG from '../images/IMG1.jpg';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import {portfolioData }from './portfolioData';
 import aos from 'aos';
 function Portfolio() {
    
+    const [start, setStart] = useState(0);
+    const [end, setEnd] = useState(3);
+
+    const handleEnd = () => {
+        if(end === 5) return 0;
+        setEnd(end => end + 2);
+        setStart(start => start + 2);
+    }
+    const handleStart = () => {
+        if(start === 0) return ;
+        setStart(start => start - 2);
+    }
 
     return (
         <div className="portfolio">
@@ -15,12 +28,12 @@ function Portfolio() {
                     Websites
                 </div>
                 <div className="header__arrows">
-                    <span className="left__icon"><FIcon.FaArrowAltCircleLeft/></span>
-                    <span className="right__icon"><FIcon.FaArrowAltCircleRight/></span>
+                    <span onClick={handleStart} className="left__icon"><FIcon.FaArrowAltCircleLeft/></span>
+                    <span onClick={handleEnd} className="right__icon"><FIcon.FaArrowAltCircleRight/></span>
                 </div>
             </div>
             <div className="portfolio__footer">
-                <div   className="card">
+                {/* <div   className="card">
                     <div className="card__header">
                         <img src={IMG} />
                     </div>
@@ -31,9 +44,24 @@ function Portfolio() {
                             <a href="#">Online link</a>
                         </div>
                     </div>
-                </div>
+                </div> */}
+                { portfolioData.slice(start,end).map((item, index) => (
 
-                <div  className="card">
+                    <div key={index} className="card">
+                    <div className="card__header">
+                        <img src={item.image} />
+                    </div>
+                    <div className="card__body">
+                        <div className="card__body__title">{item.name}</div>
+                        <div className="card__body__links">
+                            <a href="#">Github link</a>
+                            <a href={item.onlineLink}>Online link</a>
+                        </div>
+                    </div>
+                </div>
+                        ))
+                }
+                {/* <div  className="card">
                     <div className="card__header">
                         <img src={IMG} />
                     </div>
@@ -45,19 +73,7 @@ function Portfolio() {
                         </div>
                     </div>
                 </div>
-                <div  className="card">
-                    <div className="card__header">
-                        <img src={IMG} />
-                    </div>
-                    <div className="card__body">
-                        <div className="card__body__title">Project Name</div>
-                        <div className="card__body__links">
-                            <a href="#">Github link</a>
-                            <a href="#">Online link</a>
-                        </div>
-                    </div>
-                </div>
-         
+          */}
             </div>
             </div>
         </div>
---
+++ src/pages/portfolioData.js
@@ -0,0 +1,29 @@
+export const portfolioData = [
+    { 
+        image: "https://musaabdullah.github.io/images/wa3nafsak.png",
+        name: "Wa3nafsak",
+        onlineLink: "https://wa3nafsak.herokuapp.com/"
+    },
+    {
+        image: "https://musaabdullah.github.io/images/IMG33.jpg",
+        name: "ALWALI SECONDARY SCHOOL",
+        onlineLink: "https://alwalisecondaryschool.herokuapp.com/"
+    },
+    {
+        image: "https://musaabdullah.github.io/images/IMG44.png",
+        name: "Sport land",
+        onlineLink: "https://hardcore-newton-46a79a.netlify.app/views/home.html"
+    },
+    {
+        image: "https://musaabdullah.github.io/images/sudan-schools.png",
+        name: "Sudan Schools",
+        onlineLink: "https://sudan-schools.herokuapp.com/"
+    },
+    {
+        image: "https://user-images.githubusercontent.com/60151264/109391771-75abb100-7921-11eb-84f4-71294827bb9d.PNG",
+        name: "Netflix clone",
+        onlineLink: "https://netflix-clone-307e0.web.app/",
+    
+    }
+
+]
